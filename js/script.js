26ae5535f5b3ed465e62a403c94f2879bf6e5ffb
Tscharana <171447830+Tscharana@users.noreply.github.com>
2025-10-31 16:11:18 +0100

    Create 2 variables that keep track of score of user/computer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 console.log("Hello from the script file.");
 
+// Create global winning conditions for user and computer
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(humanScore);
+console.log(computerScore);
+
 // Randomly returns one of the three strings "rock", "paper" or "scissors".
 function getComputerChoice() {
     const random3 = Math.floor((Math.random() * 3) + 1);
