e29d0573a53dc000ddb4aa18455072cae9a5ed66
Tscharana <171447830+Tscharana@users.noreply.github.com>
2025-10-31 16:01:22 +0100

    Create function getComputerChoice() that randomly returns one of three strings

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log("Hello from the script file.");
+
+// Randomly returns one of the three strings "rock", "paper" or "scissors".
+function getComputerChoice () {
+    const random3 = Math.floor((Math.random() * 3) + 1);
+    if (random3 === 1) {
+            return "rock"
+        } else if (random3 === 2) {
+            return "paper"
+        } else {
+            return "scissors"
+        }
+}
+
+console.log(getComputerChoice());
